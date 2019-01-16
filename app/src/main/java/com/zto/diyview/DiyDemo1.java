2c5c915f08e6c687c3d99e3b71ef5dfbbf73dd78
wjy <376794191@qq.com>
2019-01-16 14:20:11 +0800

    删除log

--- app/src/main/java/com/zto/diyview/DiyDemo1.java
+++ app/src/main/java/com/zto/diyview/DiyDemo1.java
@@ -22,26 +22,21 @@ public class DiyDemo1 extends RelativeLayout {
     public DiyDemo1(Context context) {
         super(context);
         mContext=context;
-        Log.d(TAG, "DiyDemo1: 1111");
         init();
     }
     public DiyDemo1(Context context,AttributeSet attrs){
         super(context,attrs);
         mContext=context;
-        Log.d(TAG, "DiyDemo1: 2222");
         init();
         TypedArray array=mContext.obtainStyledAttributes(attrs,R.styleable.DiyDemo1);
         String text=array.getString(R.styleable.DiyDemo1_view_text);
 
         String hint=array.getString(R.styleable.DiyDemo1_edit_hint);
-        Log.d(TAG, "DiyDemo1: "+text+"  ");
         textView.setText(text);
         editText.setHint(hint);
     }
     public void init(){
         inflate(mContext,R.layout.diy_view_demo1,this);
-        Log.d(TAG, "init: 33333");
-
         textView=this.findViewById(R.id.demo1_text_view);
         editText=this.findViewById(R.id.demo1_edit);
         //addView(view);
